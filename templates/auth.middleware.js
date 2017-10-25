5533457e0843e61faeb09b4e1df12139829f4801
Herberts Cruz <herbertscruz@gmail.com>
2017-10-24 23:47:43 -0200

    Bug fixes.

--- templates/auth.middleware.js
+++ templates/auth.middleware.js
@@ -5,7 +5,6 @@ const options = <%= serialize(options) %>
 middleware.auth = function authMiddleware ({ store, redirect }) {
   // If user not logged in, redirect to /login
   if (!store.getters['auth/loggedIn']) {
-    console.log(options)
     return redirect(options.notLoggedInRedirectTo)
   }
 }
