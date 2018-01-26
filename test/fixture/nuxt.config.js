2cc83ab544277063f3a0a43a02c1b85a2a2fc5e8
Pooya Parsa <pooya@pi0.ir>
2018-01-26 14:41:57 +0330

    test: update nuxt.config.js

--- test/fixture/nuxt.config.js
+++ test/fixture/nuxt.config.js
@@ -1,11 +1,11 @@
+const { resolve } = require('path')
+
 module.exports = {
+  rootDir: resolve(__dirname, '../..'),
   srcDir: __dirname,
   dev: false,
   render: {
     resourceHints: false
   },
-  modules: [
-    ['@@', {}],
-    '@nuxtjs/axios'
-  ]
+  modules: ['@@', '@nuxtjs/axios'],
 }
