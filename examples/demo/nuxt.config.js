386df12dbfe1e6732db24e5a94adc878ef3c6201
Pooya Parsa <pooya@pi0.ir>
2018-02-23 14:15:23 +0330

    update demo

--- examples/demo/nuxt.config.js
+++ examples/demo/nuxt.config.js
@@ -27,44 +27,49 @@ module.exports = {
       },
       auth0: {
         _scheme: 'oauth2',
-        clientId: 'q8lDHfBLJ-Fsziu7bf351OcYQAIe3UJv',
-        authorizationUri: 'https://nuxt-auth.auth0.com/authorize',
-        userinfoUri: 'https://nuxt-auth.auth0.com/userinfo',
-        redirectUri: 'http://localhost:3000/login',
-        scopes: ['openid', 'profile', 'email']
+        authorization_endpoint: 'https://nuxt-auth.auth0.com/authorize',
+        userinfo_endpoint: 'https://nuxt-auth.auth0.com/userinfo',
+        scope: ['openid', 'profile', 'email'],
+
+        client_id: 'q8lDHfBLJ-Fsziu7bf351OcYQAIe3UJv',
+        redirect_uri: 'http://localhost:3000/login'
       },
       facebook: {
         _scheme: 'oauth2',
-        clientId: '1671464192946675',
-        authorizationUri: 'https://facebook.com/v2.12/dialog/oauth',
-        userinfoUri:
+        authorization_endpoint: 'https://facebook.com/v2.12/dialog/oauth',
+        userinfo_endpoint:
           'https://graph.facebook.com/v2.12/me?fields=about,name,picture{url},email',
-        redirectUri: 'http://localhost:3000/login',
-        scopes: ['public_profile', 'email']
+        scope: ['public_profile', 'email'],
+
+        client_id: '1671464192946675',
+        redirect_uri: 'http://localhost:3000/login'
       },
       google: {
         _scheme: 'oauth2',
-        clientId:
+        authorization_endpoint: 'https://accounts.google.com/o/oauth2/auth',
+        userinfo_endpoint: 'https://www.googleapis.com/oauth2/v3/userinfo',
+        scope: ['openid', 'profile', 'email'],
+
+        client_id:
           '956748748298-kr2t08kdbjq3ke18m3vkl6k843mra1cg.apps.googleusercontent.com',
-        authorizationUri: 'https://accounts.google.com/o/oauth2/auth',
-        userinfoUri: 'https://www.googleapis.com/oauth2/v3/userinfo',
-        redirectUri: 'http://localhost:3000/login',
-        scopes: ['openid', 'profile', 'email']
+        redirect_uri: 'http://localhost:3000/login'
+      },
+      github: {
+        _scheme: 'oauth2',
+        authorization_endpoint: 'https://github.com/login/oauth/authorize',
+        scope: ['user'],
+
+        client_id: '56ab0511106a13cbb12c',
+        redirect_uri: 'http://localhost:3000/login'
+      },
+      twitter: {
+        _scheme: 'oauth2',
+        authorization_endpoint: 'https://api.twitter.com/oauth/authorize',
+        scope: ['user'],
+
+        client_id: 'FAJNuxjMTicff6ciDKLiZ4t0D',
+        redirect_uri: 'http://localhost:3000/login'
       }
-      // github: {
-      //   _scheme: 'oauth2',
-      //   clientId: '56ab0511106a13cbb12c',
-      //   authorizationUri: 'https://github.com/login/oauth/authorize',
-      //   redirectUri: 'http://localhost:3000/login',
-      //   scopes: ['user']
-      // },
-      // twitter: {
-      //   _scheme: 'oauth2',
-      //   clientId: 'FAJNuxjMTicff6ciDKLiZ4t0D',
-      //   authorizationUri: 'https://api.twitter.com/oauth/authorize',
-      //   redirectUri: 'http://localhost:3000/login',
-      //   scopes: ['user']
-      // }
     }
   }
 }
