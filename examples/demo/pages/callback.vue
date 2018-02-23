4aa8dc4f49931aae797406be421e8e4e81d525a0
Pooya Parsa <pooya@pi0.ir>
2018-02-23 15:48:56 +0330

    update demo example

---
+++ examples/demo/pages/callback.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="container d-flex align-items-center justify-content-center flex-column">
+  <img src="~/assets/loading.svg" alt="Loading..." width="80px" />
+  Logging in...
+</div>
+</template>
+
+<style scoped>
+.container {
+  min-height: 70vh;
+}
+</style>
