nuxt-community/auth-module

commit 3ee609d6837327b98dcb38e182e1e9934c8185df

fix: don't redirect to login page if in guest mode (#385)

lib/core/middleware.js
@@ -15,22 +15,20 @@ Middleware.auth = function (ctx) {
   }
 
   const { login, callback } = ctx.app.$auth.options.redirect
+  const pageIsInGuestMode = routeOption(ctx.route, 'auth', 'guest')
+  const insideLoginPage = normalizePath(ctx.route.path) === normalizePath(login)
+  const insideCallbackPage = normalizePath(ctx.route.path) !== normalizePath(callback)
 
   if (ctx.app.$auth.$state.loggedIn) {
     // -- Authorized --
-    // Redirect to home page if:
-    // - inside login page
-    // - login page disabled
-    // - options: { auth: 'guest' } is set on the page
-    if (!login || normalizePath(ctx.route.path) === normalizePath(login) || routeOption(ctx.route, 'auth', 'guest')) {
+    if (!login || insideLoginPage || pageIsInGuestMode) {
       ctx.app.$auth.redirect('home')
     }
   } else {
     // -- Guest --
-    // Redirect to login page if not authorized and not inside callback page
     // (Those passing `callback` at runtime need to mark their callback component
     // with `auth: false` to avoid an unnecessary redirect from callback to login)
-    if (!callback || normalizePath(ctx.route.path) !== normalizePath(callback)) {
+    if (!pageIsInGuestMode && (!callback || insideCallbackPage)) {
       ctx.app.$auth.redirect('login')
     }
   }

commit 46056814c798ed6e1d35617dcd0ade9c2f0d4b43

fix: regression from #385 when callback is set to false (#391)

lib/core/middleware.js
@@ -16,19 +16,18 @@ Middleware.auth = function (ctx) {
 
   const { login, callback } = ctx.$auth.options.redirect
   const pageIsInGuestMode = routeOption(ctx.route, 'auth', 'guest')
-  const insideLoginPage = normalizePath(ctx.route.path) === normalizePath(login)
-  const insideCallbackPage = normalizePath(ctx.route.path) !== normalizePath(callback)
+  const insidePage = page => normalizePath(ctx.route.path) === normalizePath(page)
 
   if (ctx.$auth.$state.loggedIn) {
     // -- Authorized --
-    if (!login || insideLoginPage || pageIsInGuestMode) {
+    if (!login || insidePage(login) || pageIsInGuestMode) {
       ctx.$auth.redirect('home')
     }
   } else {
     // -- Guest --
     // (Those passing `callback` at runtime need to mark their callback component
     // with `auth: false` to avoid an unnecessary redirect from callback to login)
-    if (!pageIsInGuestMode && (!callback || insideCallbackPage)) {
+    if (!pageIsInGuestMode && (!callback || !insidePage(callback))) {
       ctx.$auth.redirect('login')
     }
   }