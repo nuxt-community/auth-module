79e4f813c0dc6ec9f8c435099af591cb1f874a94
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-03-16 19:37:05 -0300

    test(basic page): use `$auth.check`

--- test/fixtures/basic/pages/index.vue
+++ test/fixtures/basic/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <pre v-if="$auth.loggedIn" v-html="$auth.user"></pre>
+    <pre v-if="$auth.check" v-html="$auth.user"></pre>
     <div v-else>Please login</div>
   </div>
 </template>
