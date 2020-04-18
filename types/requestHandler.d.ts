4c407561dddf7537753a2a6cfa016e0db2d91271
jeremi gendron <gendronjeremi@gmail.com>
2020-04-18 16:31:30 -0400

    fix(#486): type $auth as Auth in RequestHandler

--- types/requestHandler.d.ts
+++ types/requestHandler.d.ts
@@ -1,6 +1,8 @@
+import Auth from "./auth"
+
 export default class RequestHandler {
     constructor(auth: any);
-    $auth: any;
+    $auth: Auth;
     _getUpdatedRequestConfig(config: any): any;
     setHeader(token: any): void;
     clearHeader(): void;
