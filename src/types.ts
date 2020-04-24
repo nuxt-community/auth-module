99ee0504fa8c626c069875ffbc99c92f62948151
pooya parsa <pooya@pi0.ir>
2020-04-24 14:15:59 +0200

    feat(types): declare $auth types

--- src/types.ts
+++ src/types.ts
@@ -38,3 +38,30 @@ export type AuthOptions = {
   refreshToken: { prefix: string }
   refreshTokenExpiration: { prefix: string }
 }
+
+declare module '@nuxt/types' {
+  interface Context {
+    $auth: Auth;
+  }
+  interface NuxtAppOptions {
+    $auth: Auth;
+  }
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $auth: Auth;
+  }
+}
+
+declare module 'vue/types/options' {
+  interface ComponentOptions<V extends Vue> {
+    auth?: true | false | 'guest';
+  }
+}
+
+declare module 'vuex/types/index' {
+  interface Store<S> {
+    $auth: any;
+  }
+}
