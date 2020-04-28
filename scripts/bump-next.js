83b07d0a75a2f33e5c1bec8fb599c042715e72f1
pooya parsa <pooya@pi0.ir>
2020-04-28 20:18:36 +0200

    chore: upate next scripts

--- scripts/bump-next.js
+++ scripts/bump-next.js
@@ -5,12 +5,9 @@ const { execSync } = require('child_process')
 const pkgPath = path.resolve(__dirname, '../package.json')
 const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
 
-const gitCommit = execSync('git --no-pager log --format="%h" -n 1').toString().trim()
+const gitRef = execSync('git --no-pager log --format="%h" -n 1').toString().trim()
+
 pkg.name = '@nuxtjs/auth-next'
-pkg.version = '5.0.0-' + Math.round((new Date() / 1000)) + '.' + gitCommit
+pkg.version = '5.0.0-' + Math.round((new Date() / 1000)) + '.' + gitRef
 
 fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2))
-
-execSync('yarn clean', { stdio: 'inherit' })
-execSync('yarn build', { stdio: 'inherit' })
-execSync('npm publish', { stdio: 'inherit' })
