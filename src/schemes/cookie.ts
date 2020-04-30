e60188963ec78aef6b8e7b74551a6b421e307840
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-04-30 11:20:39 -0300

    fix(cookie scheme): set `referer` header on mount

--- src/schemes/cookie.ts
+++ src/schemes/cookie.ts
@@ -1,3 +1,4 @@
+import requrl from 'requrl'
 import LocalScheme from './local'
 
 const DEFAULTS = {
@@ -17,8 +18,20 @@ const DEFAULTS = {
 }
 
 export default class CookieScheme extends LocalScheme {
+  public req
+
   constructor ($auth, options) {
     super($auth, options, DEFAULTS)
+
+    this.req = $auth.ctx.req
+  }
+
+  mounted () {
+    if (process.server) {
+      this.$auth.ctx.$axios.setHeader('referer', requrl(this.req))
+    }
+
+    return super.mounted()
   }
 
   check () {
