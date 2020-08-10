211f979094e2e8ed348b308bae241d5665e96c77
Maarten Van Hoof <maarten.vanhoof@isaac.nl>
2020-08-10 23:06:13 +0200

    WIP - make oidcmockserver port dynamic

--- demo/api/oidcmockserver.js
+++ demo/api/oidcmockserver.js
@@ -1,58 +1,56 @@
 const { Provider } = require('oidc-provider')
 
-const baseUrl = 'http://localhost:3000/oidc'
-const appBaseUrl = 'http://localhost:3000'
-const redirectUri = `${appBaseUrl}/callback`
+const provider = (port = 3000) => {
+  const baseUrl = `http://localhost:${port}/oidc`
+  const appBaseUrl = `http://localhost:${port}`
+  const redirectUri = `${appBaseUrl}/callback`
 
-const provider = new Provider(baseUrl, {
-  routes: {
-    authorization: '/connect/authorize',
-    token: '/connect/token',
-    userinfo: '/connect/userinfo',
-    end_session: '/connect/endsession'
-  },
-  features: {
-    devInteractions: { enabled: true },
-    clientCredentials: { enabled: false },
-    introspection: { enabled: true },
-    revocation: { enabled: true }
-  },
-  formats: {
-    AccessToken: 'jwt'
-  },
-  scopes: ['openid', 'profile', 'offline_access'],
-  claims: {
-    email: ['email'],
-    profile: ['name', 'profile']
-  },
-  clients: [
-    {
-      client_id: 'nuxt_auth_oidc_client',
-      client_secret: 'this_is_a_secret',
-      token_endpoint_auth_method: 'none',
-      grant_types: ['authorization_code', 'refresh_token'],
-      response_types: ['code'],
-      redirect_uris: [redirectUri],
-      post_logout_redirect_uris: [appBaseUrl]
-    }
-  ],
-
-  // eslint-disable-next-line require-await
-  findAccount: async (_ctx, id) => ({
-    accountId: id,
-    // eslint-disable-next-line require-await
-    claims: async () => ({
-      sub: id,
-      auth_time: new Date().getTime(),
-      idp: 'local',
-      amr: 'pwd'
-    })
-  }),
-  // eslint-disable-next-line require-await
-  issueRefreshToken: async () => {
+  return new Provider(baseUrl, {
+    routes: {
+      authorization: '/connect/authorize',
+      token: '/connect/token',
+      userinfo: '/connect/userinfo',
+      end_session: '/connect/endsession'
+    },
+    features: {
+      devInteractions: { enabled: true },
+      clientCredentials: { enabled: false },
+      introspection: { enabled: true },
+      revocation: { enabled: true }
+    },
+    formats: {
+      AccessToken: 'jwt'
+    },
+    scopes: ['openid', 'profile', 'offline_access'],
+    claims: {
+      email: ['email'],
+      profile: ['name', 'profile']
+    },
+    clients: [
+      {
+        client_id: 'nuxt_auth_oidc_client',
+        client_secret: 'this_is_a_secret',
+        token_endpoint_auth_method: 'none',
+        grant_types: ['authorization_code', 'refresh_token'],
+        response_types: ['code'],
+        redirect_uris: [redirectUri],
+        post_logout_redirect_uris: [appBaseUrl]
+      }
+    ],
+    findAccount: async (_ctx, id) => ({
+      accountId: id,
+      claims: async () => ({
+        sub: id,
+        auth_time: new Date().getTime(),
+        idp: 'local',
+        amr: 'pwd'
+      })
+    }),
+    issueRefreshToken: async () => {
     // Force refresh token issueing
-    return true
-  }
-})
+      return true
+    }
+  })
+}
 
-module.exports = provider.callback
+module.exports = (req, res, port) => provider(port).callback(req, res)
