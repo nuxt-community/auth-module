04011ca841fbe941a7ee6261f02757b48d1f046d
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-10-17 15:45:13 -0300

    test: use ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsConfig: './test/tsconfig.json'
+    }
+  },
   testEnvironment: 'node',
+  testMatch: ['**/test/**/*.ts', '**/?(*.)+(spec|test).ts'],
   transform: {
     '^.+\\.js$': 'babel-jest'
   }
