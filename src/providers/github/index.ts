750cf335178d01d1373e9dc7b3254da869d155f0
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-12-10 10:20:29 -0300

    feat(github provider): improve types

--- src/providers/github/index.ts
+++ src/providers/github/index.ts
@@ -1,7 +1,12 @@
 import { assignDefaults, addAuthorize } from '../../utils/provider'
+import ProviderPartialOptions from '../contracts/ProviderPartialOptions'
+import GithubProviderOptions from './contracts/GithubProviderOptions'
 
-export default function github(nuxt, strategy) {
-  assignDefaults(strategy, {
+export default function github(
+  nuxt,
+  strategy: ProviderPartialOptions<GithubProviderOptions>
+) {
+  const DEFAULTS: typeof strategy = {
     scheme: 'oauth2',
     endpoints: {
       authorization: 'https://github.com/login/oauth/authorize',
@@ -9,7 +14,9 @@ export default function github(nuxt, strategy) {
       userInfo: 'https://api.github.com/user'
     },
     scope: ['user', 'email']
-  })
+  }
+
+  assignDefaults(strategy, DEFAULTS)
 
   addAuthorize(nuxt, strategy)
 }
