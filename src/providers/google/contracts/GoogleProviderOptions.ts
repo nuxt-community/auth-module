98dbcfb6d6c7982d82ee8accab8a93b8dfdd830d
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-12-10 10:26:05 -0300

    feat(google provider): improve types

---
+++ src/providers/google/contracts/GoogleProviderOptions.ts
@@ -0,0 +1,8 @@
+import ProviderOptions from '../../contracts/ProviderOptions'
+import Oauth2SchemeOptions from '../../../schemes/oauth2/contracts/Oauth2SchemeOptions'
+
+export interface GoogleProviderOptions
+  extends ProviderOptions,
+    Oauth2SchemeOptions {}
+
+export default GoogleProviderOptions
--- src/providers/google/index.ts
+++ src/providers/google/index.ts
@@ -1,12 +1,19 @@
 import { assignDefaults } from '../../utils/provider'
+import ProviderPartialOptions from '../contracts/ProviderPartialOptions'
+import GoogleProviderOptions from './contracts/GoogleProviderOptions'
 
-export default function google(_nuxt, strategy) {
-  assignDefaults(strategy, {
+export default function google(
+  _nuxt,
+  strategy: ProviderPartialOptions<GoogleProviderOptions>
+) {
+  const DEFAULTS: typeof strategy = {
     scheme: 'oauth2',
     endpoints: {
       authorization: 'https://accounts.google.com/o/oauth2/auth',
       userInfo: 'https://www.googleapis.com/oauth2/v3/userinfo'
     },
     scope: ['openid', 'profile', 'email']
-  })
+  }
+
+  assignDefaults(strategy, DEFAULTS)
 }
