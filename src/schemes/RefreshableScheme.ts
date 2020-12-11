fd6ed5440cd59e8ceb5688c50293c415f18a4066
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-12-11 04:33:35 -0300

    fix(scheme): fix type `RefreshableScheme`

--- src/schemes/RefreshableScheme.ts
+++ src/schemes/RefreshableScheme.ts
@@ -1,5 +1,6 @@
 import RefreshToken from '../inc/refresh-token'
 import { HTTPResponse } from '../index'
+import RefreshController from '../inc/refresh-controller'
 import { RefreshTokenOptions } from './contracts/SchemeOptions'
 import TokenableScheme, { TokenableSchemeOptions } from './TokenableScheme'
 
@@ -11,6 +12,7 @@ export interface RefreshableScheme<
   OptionsT extends RefreshableSchemeOptions = RefreshableSchemeOptions
 > extends TokenableScheme<OptionsT> {
   refreshToken: RefreshToken
+  refreshController: RefreshController
   refreshTokens(): Promise<HTTPResponse>
 }
 
