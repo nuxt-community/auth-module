5bb3f1e02ab59f34037d01fff5f83ad14ee3a7da
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-12-14 17:39:45 -0300

    feat: export scheme and provider types at top level

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,32 @@ import _Auth from './core/auth'
 export { AxiosRequestConfig as HTTPRequest }
 export { AxiosResponse as HTTPResponse }
 
+/* Auth */
 export { _Auth as Auth }
+
+/* Providers */
+export * from './providers/index'
+
+/* Schemes */
+export * from './schemes/index'
+
+// Scheme
+export * from './schemes/Scheme'
+
+// Tokenable
+export * from './schemes/TokenableScheme'
+
+// Refreshable
+export * from './schemes/RefreshableScheme'
+
+// Local
+export * from './schemes/local'
+
+// Refresh
+export * from './schemes/refresh'
+
+// Cookie
+export * from './schemes/cookie'
+
+// Oauth2
+export * from './schemes/oauth2'
