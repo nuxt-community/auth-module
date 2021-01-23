92eabbd55580302487d483bf51df6df46aad120b
Gagandeep Singh Makhija <gagandeep.2020@gmail.com>
2021-01-23 15:30:21 +0000

    feat(oauth2): make nonce settable when response_type includes 'token' (#709)
    Co-authored-by: João Pedro Antunes Silva <joao-pedroas@hotmail.com>

--- src/schemes/oauth2.ts
+++ src/schemes/oauth2.ts
@@ -257,7 +257,7 @@ export class Oauth2Scheme<
     // Set Nonce Value if response_type contains id_token to mitigate Replay Attacks
     // More Info: https://openid.net/specs/openid-connect-core-1_0.html#NonceNotes
     // More Info: https://tools.ietf.org/html/draft-ietf-oauth-v2-threatmodel-06#section-4.6.2
-    if (opts.response_type.includes('id_token')) {
+    if (opts.response_type.includes('token')) {
       opts.nonce = _opts.nonce || randomString(10)
     }
 
