b3340d865ebd16287e5a080c4a371af224bc7da1
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-06-03 06:06:48 -0300

    refactor(core): improve vuex warn message

--- src/core/storage.ts
+++ src/core/storage.ts
@@ -134,7 +134,8 @@ export class Storage {
 
       // eslint-disable-next-line no-console
       console.warn(
-        '[AUTH] The Vuex Store is not activated. To activate it, see https://nuxtjs.org/docs/2.x/directory-structure/store'
+        '[AUTH] The Vuex Store is not activated. This might cause issues in auth module behaivior, like redirects not working propertly.' +
+        'To activate it, see https://nuxtjs.org/docs/2.x/directory-structure/store'
       )
     }
   }
