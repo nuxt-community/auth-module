c435f643d9b07355f3512dc11863af4f67ed205e
Steven Hollingsworth <shollingsworth@barracuda.com>
2022-02-14 15:07:47 -0800

    Fix linting error

--- src/schemes/openIDConnect.ts
+++ src/schemes/openIDConnect.ts
@@ -118,7 +118,7 @@ export class OpenIDConnectScheme<
       response.tokenExpired = true
       return response
     }
-    
+
     // Id token has expired. Force reset.
     if (idTokenStatus.expired()) {
       response.idTokenExpired = true
